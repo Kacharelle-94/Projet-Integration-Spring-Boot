d33200f0f21bcef1d4a53505a4438e8fbcbaf387
Kacharelle <kacharellematawe@gmail.com>
2026-08-18 01:12:46 +0100

    fix: Affichage de la maquette html

--- src/main/java/Kachou/Kacharelle3/Controller/HomeControler.java
+++ src/main/java/Kachou/Kacharelle3/Controller/HomeControler.java
@@ -1,14 +1,13 @@
 package Kachou.Kacharelle3.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-    public class HomeControler {
+@Controller // On utilise @Controller (et NON @RestController) pour charger des vues HTML
+public class HomeControler {
 
-        @GetMapping("/")
-        public String index() {
-            return "Bienvenue sur mon application Spring Boot";
-        }
+    @GetMapping("/")
+    public String index() {
+        return "index"; // Spring Boot va chercher index.html dans templates/ ou static/
     }
-
+}
